ddc5a6f3d46b7662b34a37dde3e1159ca5dcb24b
tupi <reeppi@gmail.com>
2022-09-03 08:55:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import JobList from './jobs/JobList';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+       <JobList />
+    )
+  }
+}
+
+export default App;
